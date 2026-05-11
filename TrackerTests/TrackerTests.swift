fcfa950bd7702991b10efa4bed981d2076a7e76f
Fermer3D <danil.tretyachenko.03@yandex.ru>
2026-05-12 00:31:14 +0300

    Initial Commit

---
+++ TrackerTests/TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerTests.swift
+//  TrackerTests
+//
+//  Created by Данил Третьяченко on 12.05.2026.
+//
+
+import Testing
+@testable import Tracker
+
+struct TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
